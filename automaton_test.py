16d4664ba1d08a997e9fc4b292d6de2c0bc803db
Rizal Maulana Fahmi <gorenglahayam@gmail.com>
2024-03-06 12:29:25 +0700

    format

--- automaton_test.py
+++ automaton_test.py
@@ -8,7 +8,28 @@ path = os.path.abspath("test/basic_test.py")
 print("=====================================================")
 from src.library.automation import manager
 automator = manager.AutomationManager()
+
+# set single file
 automator.set_file(file_path=path)
+
+# multi file
+for i in 5:
+    automator.add_file(file_path=path)
+
+# set single file with argv
+argv = ["--no-attribute", "https"]
+automator.set_file(file_path=path, argv=argv)
+
+for i in 5:
+    automator.add_file(file_path=path, argv=argv)
+
+# set files. gk support argv
+automator.set_files(
+    file_paths=[
+        path,path,path
+    ]
+)
+
 result = automator.run()
 print("NO ARGV TEST")
 print("Test Count:", result.test_count())
@@ -17,6 +38,7 @@ print("Effectiveness (in percent):", result.effectiveness())
 for fail in result.fails():
     print("type:", fail.type)
     print("method:", fail.method)
+    print("msg:", fail.msg)
 
 print("=====================================================")
 
