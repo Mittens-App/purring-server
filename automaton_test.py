5ebe56d96d7e83a3ac0da96665f68c8d298944e2
Rizal Maulana Fahmi <gorenglahayam@gmail.com>
2024-03-09 12:10:16 +0700

    test

--- automaton_test.py
+++ automaton_test.py
@@ -17,17 +17,27 @@ path2 = "D:/rizal/projects/purring-test/my/my/purring/test4_basic_headless2.py"
 path3 = "D:/rizal/projects/mittens/purring_server/test/basic_test.py"
 path4 = "C:/Users/USER/Documents/run_thisxy.py"
 path5 = "D:/rizal/projects/mittens/purring_server/src/library/automation/result.py"
-testclass = automator.get_functions(file_path=path5)
-print("classname:", testclass.classname)
-print("path:", testclass.path)
-for func in testclass.functions:
-    print("function|comment:", func.name, "|", func.comment)
+# testclass = automator.get_functions(file_path=path5)
+# print("classname:", testclass.classname)
+# print("path:", testclass.path)
+# for func in testclass.functions:
+#     print("function|comment:", func.name, "|", func.comment)
 # ================== END ==================
 
+# def split_list(alist, wanted_parts=1):
+#     length = len(alist)
+#     return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+#              for i in range(wanted_parts) ]
+
+# A = [0,1,2,3,4,5,6,7,8,9]
+
+# print( split_list(A, wanted_parts=1))
+# print (split_list(A, wanted_parts=2))
+# print (split_list(A, wanted_parts=8))
 
 
 # ============= set single file ============
-# automator.set_file(file_path=path)
+# automator.set_file(file_path=path2)
 
 # ================== END ==================
 
@@ -35,7 +45,7 @@ for func in testclass.functions:
 
 # ============= multi file ============
 ## example 1
-# automator.add_file(path2).add_file(path3).add_file(path2)
+automator.add_file(path2,["-aaa"]).add_file(path3,["-aaa"]).add_file(path4,["-aaa"]) #.add_file(path3,["-aaa"])
 
 ## example 2
 # for i in 5:
@@ -69,14 +79,23 @@ for func in testclass.functions:
 
 
 # ============= run ============
-# result = automator.run()
-# print("Test Count:", result.test_count())
-# print("Test Success Status:", result.is_success())
-# print("Effectiveness (in percent):", result.effectiveness())
-# for fail in result.fails():
-#     print("type:", fail.type)
-#     print("method:", fail.method)
-#     # print("msg:", fail.msg)
+result = automator.run()
+print("Test Count:", result.test_count())
+print("Test Success Status:", result.is_success())
+print("Effectiveness (in percent):", result.effectiveness())
+print("Duration (in seconds):", result.duration())
+print("ResultCases:")
+for resultcase in result.resultcase():
+    print("      - classname:", resultcase.classname)
+    print("        duration:", resultcase.duration)
+    print("        file_path:", resultcase.file_path)
+    print("        test_count:", resultcase.test_count)
+    print("        success_status:", resultcase.success)
+    print("        fails:")
+    for fail in resultcase.fails():
+        print("              - type:", fail.type)
+        print("                method:", fail.method)
+        # print("                msg:", fail.msg)
 
 # print("=====================================================")
 
--- test/basic_test.py
+++ test/basic_test.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import sys
 
 class MyTestCase(unittest.TestCase):
 
@@ -10,27 +11,29 @@ class MyTestCase(unittest.TestCase):
         self.browser = webdriver.Firefox(options=options)
         self.addCleanup(self.browser.quit)
 
-    def test_page_title(self):
+    def test_page_title2(self):
         """
         Test open googl.
         """
         self.browser.get('http://www.google.com')
         self.assertIn('Google', self.browser.title)
 
-    def test_page_yahoo(self):
-        """
-        Test open Yau.
-        """
-        self.browser.get('http://www.yahoo.xcom')
-        # self.assertIn('Google', self.browser.title)
-        self.fail('gagal ambil cumi2')
+    # def test_page_yahoo2(self):
+    #     """
+    #     Test open Yau.
+    #     """
+    #     self.browser.get('http://www.yahoo.xcom')
+    #     # self.assertIn('Google', self.browser.title)
+    #     self.fail('gagal ambil cumi2')
 
-    def test_pagii(self):
-        """
-        Test open xx.
-        """
-        self.browser.get('https://docs.python.org')
-        self.assertEqual('python', self.browser.title)
+    # def test_pagii2(self):
+    #     """
+    #     Test open xx.
+    #     """
+    #     print( 'Argument Listx:', str(sys.argv))
+    #     self.browser.get('https://docs.python.org')
+    #     self.assertEqual('python', self.browser.title)
 
 if __name__ == '__main__':
+    print(sys.argv[1])
     unittest.main(verbosity=2)
